47f5f231621ca659bd09eb7b7f46b39dde9f5e2b
Daniel-Chacha <danmwita355@gmail.com>
2025-06-14 18:57:01 +0300

    corrected types for animation, movies and series page.tsx

--- src/app/pages/animations/[series_id]/[season_number]/[episode_number]/page.tsx
+++ src/app/pages/animations/[series_id]/[season_number]/[episode_number]/page.tsx
@@ -1,22 +1,27 @@
+'use client';
 import { Screen } from "@/src/app/components/screen";
 
-type PageProps={
-    params:{
-        series_id: string;
-        season_number: string;
-        episode_number: string;
-    }
-}
-export default  function ScreenPage({params}: PageProps){
-    const seriesId  = parseInt(params.series_id);
-    const seasonNumber = parseInt(params.season_number, 10); 
-    const episodeNumber = parseInt(params.episode_number);
+export default function ScreenPage({
+  params,
+}: {
+  params: {
+    series_id: number;
+    season_number: number;
+    episode_number: number;
+  };
+}) {
+//   const seriesId = parseInt(params.series_id);
+//   const seasonNumber = parseInt(params.season_number, 10);
+//   const episodeNumber = parseInt(params.episode_number);
 
-    const url= `https://vidsrc.xyz/embed/tv/${seriesId}/${seasonNumber}/${episodeNumber}`;
+ const url= `https://vidsrc.xyz/embed/tv/${params.series_id}/${params.season_number}/${params.episode_number}`
 
-    return(
-        <div className="bg-black min-h-screen relative">
-            <Screen url={url} />
-        </div>
-    )
+  return (
+    <div className="bg-black min-h-screen relative">
+      <Screen url={url} />
+    </div>
+  );
 }
+
+
+
--- src/app/pages/series/[series_id]/[season_number]/[episode_number]/page.tsx
+++ src/app/pages/series/[series_id]/[season_number]/[episode_number]/page.tsx
@@ -3,17 +3,17 @@ import { Screen } from "@/src/app/components/screen";
 
 type PageProps={
     params:{
-        series_id: string;
-        season_number: string;
-        episode_number: string;
+        series_id: number;
+        season_number: number;
+        episode_number: number;
     }
 }
 export default  function ScreenPage({params}: PageProps){
-    const seriesId  = parseInt(params.series_id);
-    const seasonNumber = parseInt(params.season_number, 10); 
-    const episodeNumber = parseInt(params.episode_number);
+    // const seriesId  = parseInt(params.series_id);
+    // const seasonNumber = parseInt(params.season_number, 10); 
+    // const episodeNumber = parseInt(params.episode_number);
 
-    const url= `https://vidsrc.xyz/embed/tv/${seriesId}/${seasonNumber}/${episodeNumber}`;
+    const url= `https://vidsrc.xyz/embed/tv/${params.series_id}/${params.season_number}/${params.episode_number}`;
 
     return(
         <div className="bg-black min-h-screen relative">
